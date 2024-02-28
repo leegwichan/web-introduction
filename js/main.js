95b2be2ab6d78eb4dd2598253ed322020b8a8c0e
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-28 20:57:03 +0900

    feat : 2단계 - 등록 기능 구현

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const enrollButton = document.getElementById('enroll');
+enrollButton.onclick = () => enroll();
+
+const cancelButton = document.getElementById('reset');
+cancelButton.onclick = () => reset();
+
+function enroll() {
+    const visitorName = document.getElementById('visitorName').value;
+    if (visitorName.length == 0) {
+        alert('이름을 입력해 주세요');
+        return;
+    }
+
+    const selectedElements = document.querySelectorAll('input[name="movie"]:checked');
+    const selectedElementsNum = selectedElements.length;
+
+    alert( visitorName + '님, 저와 ' + selectedElementsNum + '개의 취향이 같으시네요!');
+}
+
+function reset() {
+    alert('Reset Button clicked!');
+}
